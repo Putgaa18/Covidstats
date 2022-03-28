b902c4d3366ef833229d585e04cf1814eea81ce3
Putgaa18 <putgaa18@htl-kaindorf.at>
2022-03-28 15:04:25 +0200

    putgaa18 28-03-2022

---
+++ src/main/java/com/example/Covid_Stats/CovidResource.java
@@ -0,0 +1,30 @@
+package com.example.Covid_Stats;
+
+import com.example.Covid_Stats.db.CovidDB;
+import com.example.Covid_Stats.singlecountry.Stats;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.NoSuchElementException;
+
+@Path("/stats")
+public class CovidResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{c}")
+    public Response loadData(@PathParam("c") String country)
+    {
+        try {
+                CovidDB.getInstance().loadCovidData(country);
+                Stats stats = CovidDB.getInstance().getStats();
+                return Response.ok(stats).build();
+        }
+        catch (NoSuchElementException nsee) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
--- src/main/java/com/example/Covid_Stats/singlecountry/Stats.java
+++ src/main/java/com/example/Covid_Stats/singlecountry/Stats.java
@@ -6,6 +6,7 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
@@ -24,48 +25,23 @@ import java.util.Arrays;
 import java.util.List;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@XmlRootElement(name = "root")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Stats {
-    @XmlElement(name = "0")
-    private SpecifiedCountry sp;
-
-    public static void main(String[] args) {
-        try {
-            HttpResponse response = Unirest.get("https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p." +
-                    "rapidapi.com/api/npm-covid-data/country-report-iso-based/Austria/aut")
-                    .header("x-rapidapi-host", "vaccovid-coronavirus-vaccine-and-treatment-tracker.p." +
-                            "rapidapi.com")
-                    .header("x-rapidapi-key", "b9e5b6b837msh0d164cdd8603773p15f8e8jsnfd2a93515373")
-                    .asString();
-
-            String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
-                    " <root>\n" +
-                    "     <0>\n" +
-                    "         <Country>Canada</Country>\n" +
-                    "     </0>\n" +
-                    " </root>";
-
-            //InputStream is = response.getRawBody();
-            String str = (String) response.getBody();
-            System.out.println(str);
-            //Stats stats2 = JAXB.unmarshal(str, Stats.class);
-            //System.out.println(stats2);
-            String jsonStr = str.substring(1, str.length() - 1);
-            JSONObject json = new JSONObject(jsonStr);
-            String xmlStr = XML.toString(json);
-            System.out.println(xmlStr);
-
-            JAXBContext  jaxbContext = JAXBContext.newInstance(Stats.class);
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-
-            StringReader reader = new StringReader(xmlStr);
-            Stats stats3 = (Stats) unmarshaller.unmarshal(reader);
-            System.out.println(stats3);
-
-            //Stats stats = JAXB.unmarshal(xmlStr, Stats.class);
-            //System.out.println(stats);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    @XmlElement(name = "Country")
+    private String country;
+    @XmlElement(name = "ThreeLetterSymbol")
+    private String threeLetter;
+    @XmlElement(name = "TotalCases")
+    private String totalcases;
+    @XmlElement(name = "NewCases")
+    private String newcases;
+    @XmlElement(name = "TotalDeaths")
+    private String totaldeaths;
+    @XmlElement(name = "NewDeaths")
+    private String newdeaths;
+    private String rank;
 }

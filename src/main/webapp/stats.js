e5c533b1b0ab550d8e14215415b00e89b085171c
Putgaa18 <putgaa18@htl-kaindorf.at>
2022-03-28 15:25:37 +0200

    putgaa18 2nd version 28-03-2022

---
+++ src/main/webapp/stats.js
@@ -0,0 +1,30 @@
+function getStats()
+{
+    var inputCountry = document.getElementById("inputCountry").value;
+    fetch('./api/stats/' + inputCountry)
+        .then(
+            function(response ){
+                if(response.status !== 200)
+                {
+                    console.log('Looks like there was a problem: ' + response.status);
+                    return;
+                }
+                response.json().then(function(data){
+                    var info = document.getElementById("outData");
+                        info.innerText = "Country: " + data.country;
+                        info.innerHTML += "<br/>";
+                        info.innerText += "ThreeLetter: " + data.threeLetter;
+                        info.innerHTML += "<br/>";
+                        info.innerText += "Total cases: " + data.totalcases;
+                        info.innerHTML += "<br/>";
+                        info.innerText += "New cases: " + data.newcases;
+                        info.innerHTML += "<br/>";
+                        info.innerText += "Total deaths: " + data.totaldeaths;
+                        info.innerHTML += "<br/>";
+                        info.innerText += "New deaths: " + data.newdeaths;
+                        info.innerHTML += "<br/>";
+                        info.innerText += "Rank: " + data.rank;
+                })
+            }
+        );
+}
